78bb0989cb24bf5ddd98aebe0d055494c410aa69
Growthh <magnum94.rus@gmail.com>
2018-11-08 20:07:07 +0300

    First commit

--- app/src/main/java/com/example/growthh/main_project1/MainActivity.java
+++ app/src/main/java/com/example/growthh/main_project1/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.growthh.main_project1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+        public void onButtonClick (View v) {
+            EditText num1 = (EditText)findViewById(R.id.num1);
+            EditText num2 = (EditText)findViewById(R.id.num2);
+            TextView resText = (TextView)findViewById(R.id.result);
+
+            int number1 = Integer.parseInt(num1.getText().toString());
+            int number2 = Integer.parseInt(num2.getText().toString());
+            int result = number1 + number2;
+            resText.setText(Integer.toString(result));
+        }
+    }
